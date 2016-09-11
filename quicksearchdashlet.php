6609123f170baad489dcb59bfc1f5c825d8bca9f
Kevin Levie <kevin@levity.nl>
2016-09-11 05:48:29 +0200

    Remove 4.6 branch, remove obselete function, other trivial changes

--- quicksearchdashlet.php
+++ quicksearchdashlet.php
@@ -3,6 +3,22 @@
 require_once 'quicksearchdashlet.civix.php';
 
 /**
+ * Only implemented function in this file: add widget by default on first
+ * initalisation. On extension enable action (Dashboard.Create) is in the Upgrader class.
+ * @param array $availableDashlets
+ * @param array $defaultDashlets
+ */
+function quicksearchdashlet_civicrm_dashboard_defaults($availableDashlets, &$defaultDashlets){
+    $contactID = CRM_Core_Session::singleton()->get('userID');
+    $defaultDashlets['quicksearch'] = array(
+        'dashboard_id' => $availableDashlets['quicksearch']['id'],
+        'is_active' => 1,
+        'column_no' => '0',
+        'contact_id' => $contactID,
+    );
+}
+
+/**
  * Implementation of hook_civicrm_config
  * @param $config array(string)
  */
@@ -23,7 +39,7 @@ function quicksearchdashlet_civicrm_xmlMenu(&$files) {
  * (Parent function executes Upgrader->install)
  */
 function quicksearchdashlet_civicrm_install() {
-  return _quicksearchdashlet_civix_civicrm_install();
+    _quicksearchdashlet_civix_civicrm_install();
 }
 
 /**
@@ -31,7 +47,7 @@ function quicksearchdashlet_civicrm_install() {
  * (Parent function executes Upgrader->uninstall)
  */
 function quicksearchdashlet_civicrm_uninstall() {
-	return _quicksearchdashlet_civix_civicrm_uninstall();
+    _quicksearchdashlet_civix_civicrm_uninstall();
 }
 
 /**
@@ -50,12 +66,9 @@ function quicksearchdashlet_civicrm_disable() {
 
 /**
  * Implementation of hook_civicrm_upgrade
- *
  * @param $op string, the type of operation being performed; 'check' or 'enqueue'
  * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
+ * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending) for 'enqueue', returns void
  */
 function quicksearchdashlet_civicrm_upgrade($op, CRM_Queue_Queue $queue = null) {
 	return _quicksearchdashlet_civix_civicrm_upgrade($op, $queue);
@@ -68,7 +81,7 @@ function quicksearchdashlet_civicrm_upgrade($op, CRM_Queue_Queue $queue = null) 
  * is installed, disabled, uninstalled.
  */
 function quicksearchdashlet_civicrm_managed(&$entities) {
-	return _quicksearchdashlet_civix_civicrm_managed($entities);
+    _quicksearchdashlet_civix_civicrm_managed($entities);
 }
 
 /**
@@ -82,28 +95,3 @@ function quicksearchdashlet_civicrm_caseTypes(&$caseTypes) {
 	_quicksearchdashlet_civix_civicrm_caseTypes($caseTypes);
 }
 
-function quicksearchdashlet_civicrm_dashboard_defaults($availableDashlets, &$defaultDashlets){
-  $contactID = CRM_Core_Session::singleton()->get('userID');
-  $defaultDashlets['quicksearch'] = array(
-    'dashboard_id' => $availableDashlets['quicksearch']['id'],
-    'is_active' => 1,
-    'column_no' => '0',
-    'contact_id' => $contactID,
-  );
-}
-
-/**
- * Function to fetch dashlets (used in the hooks above)
- */
-function quicksearchdashlet_fetch_dashlets() {
-	$data = civicrm_api3('Dashboard', 'get');
-	if ($data['is_error'])
-		throw new Exception("Could not initialize dashlets. API error: " . $data['error_message']);
-
-	$dashlet_data = array();
-	foreach ($data['values'] as $dashlet) {
-		$dashlet_data[$dashlet['id']] = $dashlet['name'];
-	}
-
-	return $dashlet_data;
-}
